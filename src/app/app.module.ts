2bd48f60598c7196fd3cd1efcbbeb1f240177921
Darchevil <yderneville@gmail.com>
2023-12-05 23:26:15 +0100

    Adding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './component/home/home.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { SixQuiPrendComponent } from './pages/six-qui-prend/six-qui-prend.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, SixQuiPrendComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
     MatButtonModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
